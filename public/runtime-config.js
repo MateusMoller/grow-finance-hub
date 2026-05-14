4af7088c2d0d554d1713c9167987780af32e383f
mateus <mateushmoller@gmail.com>
2026-05-14 08:39:23 -0300

    ajuste no controle das notificações

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -2,4 +2,5 @@ window.__GROW_RUNTIME_CONFIG__ = window.__GROW_RUNTIME_CONFIG__ || {
   VITE_SUPABASE_PROJECT_ID: "vgkmcerjlwnzbiukinhd",
   VITE_SUPABASE_PUBLISHABLE_KEY: "sb_publishable_yTevo6bvvk8CaNWLHhNICA_BHR-Mewi",
   VITE_SUPABASE_URL: "https://vgkmcerjlwnzbiukinhd.supabase.co",
+  VITE_WEB_PUSH_PUBLIC_KEY: "BEy4g-7QtN-OpJZrX4Xxo_sv8AdNjRWngzu3s4w8GKLg-n8OGP-zgrVwR8KASHkv1xXK0_zWp1cNpbGSpQVFpd0",
 };
